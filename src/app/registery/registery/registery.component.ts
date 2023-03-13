646917811be3ad7c6f34c3f9a2be63cfb45c3069
Alireza <alireza.izadi@gmail.com>
2023-03-13 19:54:50 +0330

    update

---
+++ src/app/registery/registery/registery.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registery',
+  templateUrl: './registery.component.html',
+  styleUrls: ['./registery.component.scss']
+})
+export class RegisteryComponent {
+
+}
