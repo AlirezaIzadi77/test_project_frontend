f277c3fa84249f055c46453129391852f1f230cb
Alireza <alireza.izadi@gmail.com>
2023-03-13 19:46:50 +0330

    create login & registry page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {NotFoundComponent} from "./notFoundPage/not-found/not-found.component";
+import {RegisteryComponent} from "./registery/registery/registery.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'registry', component: RegisteryComponent},
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
